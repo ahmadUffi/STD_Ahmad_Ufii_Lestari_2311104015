467b9ee8c03166bfccd81379e8549cb6d7fe66a2
ahmadUffi <ahmaduffi45@gmail.com>
2024-12-15 11:06:46 +0700

    submit

---
+++ 14_Graph/guided/guided.cpp
@@ -0,0 +1,175 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct ElmNode;
+
+struct ElmEdge
+{
+    ElmNode *Node;
+    ElmEdge *Next;
+};
+
+struct ElmNode
+{
+    char info;
+    bool visited;
+    ElmEdge *firstEdge;
+    ElmNode *Next;
+};
+
+struct Graph
+{
+    ElmNode *first;
+};
+
+void CreateGraph(Graph &G)
+{
+    G.first = NULL;
+}
+
+void insertNode(Graph &G, char X)
+{
+    ElmNode *newNode = new ElmNode;
+    newNode->info = X;
+    newNode->visited = false;
+    newNode->firstEdge = NULL;
+    newNode->Next = NULL;
+
+    if (G.first == NULL)
+    {
+        G.first = newNode;
+    }
+    else
+    {
+        ElmNode *temp = G.first;
+        while (temp->Next != NULL)
+        {
+            temp = temp->Next;
+        }
+        temp->Next = newNode;
+    }
+}
+
+void ConnectNode(ElmNode *N1, ElmNode *N2)
+{
+    ElmEdge *newEdge = new ElmEdge;
+    newEdge->Node = N2;
+    newEdge->Next = N1->firstEdge;
+    N1->firstEdge = newEdge;
+}
+
+void PrintInfoGraph(Graph G)
+{
+    ElmNode *temp = G.first;
+    while (temp != NULL)
+    {
+        cout << temp->info << " ";
+        temp = temp->Next;
+    }
+    cout << endl;
+}
+
+void ResetVisited(Graph &G)
+{
+    ElmNode *temp = G.first;
+    while (temp != NULL)
+    {
+        temp->visited = false;
+        temp = temp->Next;
+    }
+}
+
+void PrintDFS(Graph &G, ElmNode *N)
+{
+    if (N == NULL)
+    {
+        return;
+    }
+    N->visited = true;
+    cout << N->info << " ";
+    ElmEdge *edge = N->firstEdge;
+    while (edge != NULL)
+    {
+        if (!edge->Node->visited)
+        {
+            PrintDFS(G, edge->Node);
+        }
+        edge = edge->Next;
+    }
+}
+
+void PrintBFS(Graph G, ElmNode *N)
+{
+    if (N == NULL)
+    {
+        return;
+    }
+
+    queue<ElmNode *> q;
+    q.push(N);
+    N->visited = true;
+
+    while (!q.empty())
+    {
+        ElmNode *current = q.front();
+        q.pop();
+        cout << current->info << " ";
+
+        ElmEdge *edge = current->firstEdge;
+        while (edge != NULL)
+        {
+            if (!edge->Node->visited)
+            {
+                edge->Node->visited = true;
+                q.push(edge->Node);
+            }
+            edge = edge->Next;
+        }
+    }
+}
+
+int main()
+{
+    Graph G;
+    CreateGraph(G);
+
+    insertNode(G, 'A');
+    insertNode(G, 'B');
+    insertNode(G, 'C');
+    insertNode(G, 'D');
+    insertNode(G, 'E');
+    insertNode(G, 'F');
+    insertNode(G, 'G');
+    insertNode(G, 'H');
+
+    ElmNode *A = G.first;
+    ElmNode *B = A->Next;
+    ElmNode *C = B->Next;
+    ElmNode *D = C->Next;
+    ElmNode *E = D->Next;
+    ElmNode *F = E->Next;
+    ElmNode *G_node = F->Next;
+    ElmNode *H = G_node->Next;
+
+    ConnectNode(A, B);
+    ConnectNode(A, C);
+    ConnectNode(B, D);
+    ConnectNode(B, E);
+    ConnectNode(C, F);
+    ConnectNode(C, G_node);
+    ConnectNode(D, H);
+
+    cout << "DFS Traversal: ";
+    ResetVisited(G);
+    PrintDFS(G, A);
+    cout << endl;
+
+    cout << "BFS Traversal: ";
+    ResetVisited(G);
+    PrintBFS(G, A);
+    cout << endl;
+
+    return 0;
+}
